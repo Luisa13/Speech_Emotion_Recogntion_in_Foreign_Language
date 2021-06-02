99cb3957d1b23731319fe60960984fc304967b3f
Luisa <luisatrece@gmail.com>
2021-06-03 00:16:28 +0200

    Initialize library

---
+++ src/SpeechFeatures.py
@@ -0,0 +1,154 @@
+import librosa
+import numpy as np
+from tqdm import tqdm
+
+class SpeechFeatures():
+    def __init__(self):
+        print()
+
+    def get_features(df, modifier):
+        '''
+        Extrae las caracteristicas de un conjunto de pistas de audio a
+        partir de un dataframe usando librosa
+
+        Aguments
+        ---------
+          df : dataframe
+          Dataframe que contiene el path donde se encuentra la pista de audio
+          modifier: Function
+          Funcion que modifica los datos
+
+        Return
+        -------
+         data: np.array
+         Caracteristicas extraidas
+
+        '''
+        bar_data_range = tqdm(range(len(df)))
+        data = pd.DataFrame(columns=['data'])
+        for index in bar_data_range:
+            data_features = modifier(df.path[index])
+            data.loc[index] = [data_features]
+
+        return data
+
+    def get_features_single_file(pathfile):
+        '''
+        Extrae las caracteristicas  de una unica pista de audio usando MFCC
+        a traves de librosa.
+
+        Aguments
+        ---------
+          pathfile: str
+            Path del archivo del que se extraeran las caracteristicas
+
+        Return
+        -------
+          data_features
+
+        '''
+        X, sample_rate = librosa.load(pathfile, res_type='kaiser_fast')
+        mfcc = librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40)
+        data_features = np.mean(mfcc.T, axis=0)
+
+        return data_features
+
+    def get_features_white_noise(pathfile):
+        '''
+        Extrae las caracteristicas  de una unica pista de audio usando MFCC
+        a traves de librosa habiendoles aplicado ruido blanco.
+
+        Aguments
+        ---------
+          pathfile: str
+            Path del archivo del que se extraeran las caracteristicas
+
+        Return
+        -------
+          data_features
+
+        '''
+        X, sample_rate = librosa.load(pathfile, res_type='kaiser_fast')
+        # X = librosa.core.load(random_sample)[0]
+
+        x_data_wn = white_noise(X)
+        mfcc = librosa.feature.mfcc(y=x_data_wn, sr=sample_rate, n_mfcc=40)
+        data_features = np.mean(mfcc.T, axis=0)
+
+        return data_features
+
+    def get_features_shiftted(pathfile):
+        '''
+        Extrae las caracteristicas  de una unica pista de audio usando MFCC
+        a traves de librosa habiendo desplazado las frecuencias perviamente.
+
+        Aguments
+        ---------
+          pathfile: str
+            Path del archivo del que se extraeran las caracteristicas
+
+        Return
+        -------
+          data_features
+
+         '''
+        X, sample_rate = librosa.load(pathfile, res_type='kaiser_fast')
+        # X = librosa.core.load(random_sample)[0]
+
+        x_data_wn = shift_audio_sample(X)
+        mfcc = librosa.feature.mfcc(y=x_data_wn, sr=sample_rate, n_mfcc=40)
+        data_features = np.mean(mfcc.T, axis=0)
+
+        return data_features
+
+    def get_features_pitch(self, pathfile):
+        '''
+        Aplica modulacion del tono en cada muestra y despues extrae las caracteristicas
+        usando el algoritmo MFCC
+
+        Aguments
+        ---------
+        pathfile: str
+            Path del archivo del que se extraeran las caracteristicas
+
+        Return
+        -------
+        data_features
+
+        '''
+        X, sample_rate = librosa.load(pathfile, res_type='kaiser_fast')
+        # X = librosa.core.load(random_sample)[0]
+
+        x_data_wn = self.pitch_shift(X)
+        mfcc = librosa.feature.mfcc(y=x_data_wn, sr=sample_rate, n_mfcc=40)
+        data_features = np.mean(mfcc.T, axis=0)
+
+        return data_features
+
+
+    def pitch_shift(self, data, bins_per_octave=12, pitch_pm=2):
+        '''
+        Modula el tono y modifica la velocidad de una pista de audio
+        '''
+        pitch_change = pitch_pm * 2 * (np.random.uniform())
+        data_pitch = librosa.effects.pitch_shift(data.astype('float64'), 16000, n_steps=pitch_change,bins_per_octave=bins_per_octave)
+
+        return data_pitch
+
+    def white_noise(data):
+        '''
+        Agrega ruido blanco a una pista de audio
+        '''
+        wn_spectrum = np.random.randn(len(data))
+        data_wn = data + 0.005 * wn_spectrum
+        return data_wn
+
+    def shift_audio_sample(data, f_low=-5, f_high=5, spec=1):
+        '''
+        Desplaza una se;al acustica en un rango de frecuencia
+        '''
+        d_range = int(np.random.uniform(low=f_low, high=f_high) * spec)
+        data_shiftted = np.roll(data, d_range)
+
+        return data_shiftted
+
